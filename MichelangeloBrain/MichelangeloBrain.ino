b7f3fafb36acf13002a8b8f332f8b770c30e2ea7
Ana Dulce <123557548+Anadulce57@users.noreply.github.com>
2026-08-28 21:11:08 -0300

    Add files via upload

---
+++ MichelangeloBrain/MichelangeloBrain.ino
@@ -0,0 +1,12 @@
+// Programação para o robô Desvia obstáculos
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+       
